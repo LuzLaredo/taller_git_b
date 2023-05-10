257e5cc041c32f330bbbd3ce861241c622715c77
LuzLaredo <lhl@est.univalle.edu>
2023-05-10 14:25:36 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hello, World!");
             int num,num2,sum;
+            string name = "luzlaredo";
+            string apellido = "esmeralda";
+            string nombrecompleto = name + apellido;
             Console.WriteLine("ingrese un numero");
             num=int.Parse(Console.ReadLine());
             num2 = int.Parse(Console.ReadLine());
