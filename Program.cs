145a950bfb05fbff174bb019246fc1ce2aad8713
LuzLaredo <lhl@est.univalle.edu>
2023-05-10 14:18:45 -0400

    mi primer hola mundo!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace nro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int num,num2,sum;
+            Console.WriteLine("ingrese un numero");
+            num=int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            sum = num + num2;
+
+        }
+    }
+}
